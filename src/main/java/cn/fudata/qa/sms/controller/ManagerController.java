f0f894b2135c10cc8b9c132e76e991f071366d3a
youlong <youlong@wacai.com>
2019-01-29 14:52:23 +0800

    swagger 屏蔽初始化接口

--- src/main/java/cn/fudata/qa/sms/controller/ManagerController.java
+++ src/main/java/cn/fudata/qa/sms/controller/ManagerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author youlong
  * date 2019/1/29
  */
-@Api(value = "/manager", tags = "猫池操作")
+//@Api(value = "/manager", tags = "猫池操作")
 @RestController
 @RequestMapping(value = "/manager")
 public class ManagerController {
@@ -21,7 +21,7 @@ public class ManagerController {
     @Autowired
     ManagerService managerService;
 
-    @ApiOperation(value = "初始化猫池端口号和手机号对应关系")
+//    @ApiOperation(value = "初始化猫池端口号和手机号对应关系, 理论上仅需在猫池组件完成后操作一次，无需频繁操作")
     @PostMapping(value = "/init")
     public boolean init(){
         try {

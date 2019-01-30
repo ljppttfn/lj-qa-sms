8d01600d5f5131cf5b027e85d0ced57c12d4ad35
youlong <youlong@wacai.com>
2019-01-30 10:49:41 +0800

    屏蔽初始化接口在swagger UI 上展示2

--- src/main/java/cn/fudata/qa/sms/controller2/ManagerController.java
+++ src/main/java/cn/fudata/qa/sms/controller2/ManagerController.java
@@ -2,18 +2,18 @@ package cn.fudata.qa.sms.controller2;
 
 
 import cn.fudata.qa.sms.service.ManagerService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * @author youlong
  * date 2019/1/29
  */
 //@Api(value = "/manager", tags = "猫池操作")
+@ApiIgnore
 @RestController
 @RequestMapping(value = "/manager")
 public class ManagerController {
@@ -21,13 +21,13 @@ public class ManagerController {
     @Autowired
     ManagerService managerService;
 
-//    @ApiOperation(value = "初始化猫池端口号和手机号对应关系, 理论上仅需在猫池组件完成后操作一次，无需频繁操作")
+    //    @ApiOperation(value = "初始化猫池端口号和手机号对应关系, 理论上仅需在猫池组件完成后操作一次，无需频繁操作")
     @PostMapping(value = "/init")
-    public boolean init(){
+    public boolean init() {
         try {
             managerService.init_all_cardPositionPortNum();
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
